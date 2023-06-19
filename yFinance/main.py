8eefc084cfc8cb4feeabbd6e252155796ec9f8c2
= <=>
2023-06-19 19:39:56 -0400

    test

--- yFinance/main.py
+++ yFinance/main.py
@@ -3,18 +3,10 @@ import plotly.graph_objects as go
 import pandas as pd
 
 if __name__ == '__main__':
-    msft = yf.Ticker("MSFT")
+    msft = yf.Ticker("SPY")
 
     # get historical market data
-    hist = msft.history(period="100d")
-
-    #hist['Date'] = hist['Date'].apply(lambda x: x[:10])
-
-    print(isinstance(hist, pd.DataFrame))
-
-    print(hist.columns, hist.index)
-    print(hist.size)
-    print(hist)
+    hist = msft.history(period="1mo")
 
     fig = go.Figure(data=[go.Candlestick(x=hist.index.tolist(),
                 open=hist['Open'],
